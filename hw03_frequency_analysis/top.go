dba38922bdf60e65c3235c337407dd3d2304a6ae
Ilya Sitnikov <sitnikovik@yandex.ru>
2024-06-14 12:45:15 +0300

    HW3 is completed

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,90 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"regexp"
+	"sort"
+	"strings"
+)
+
+type kv struct {
+	k string
+	v int
+}
+
+func Top10(s string) []string {
+	words := strings.Fields(s)
+	n := len(words)
+	if n == 0 {
+		return []string{}
+	}
+
+	kvs := makeKvs(words)
+	sortKvs(kvs)
+
+	return getFirstUpTo(kvs, 10)
+}
+
+func makeKvs(ss []string) []kv {
+	n := len(ss)
+	kvs := make([]kv, n)
+	idxmap := make(map[string]int, n)
+	excludedWords := make(map[string]struct{}, n)
+	rexp := regexp.MustCompile(`^[a-zA-Zа-яА-Я]|[^\w-]|^\d+`)
+
+	for i := 0; i < n; i++ {
+		word := prepareWord(ss[i])
+
+		if _, ok := excludedWords[word]; ok {
+			continue
+		}
+		if !isStringValid(rexp, word) {
+			excludedWords[word] = struct{}{}
+			continue
+		}
+
+		if _, ok := idxmap[word]; ok {
+			kvs[idxmap[word]].v++
+			continue
+		}
+
+		idxmap[word] = i
+		kvs[i] = kv{
+			k: word,
+			v: 1,
+		}
+	}
+
+	return kvs
+}
+
+func prepareWord(s string) string {
+	return strings.TrimRight(strings.ToLower(s), ".")
+}
+
+func isStringValid(rexp *regexp.Regexp, s string) bool {
+	return rexp.MatchString(s)
+}
+
+func sortKvs(kvs []kv) {
+	sort.Slice(kvs, func(i, j int) bool {
+		return kvs[i].v > kvs[j].v || (kvs[i].v == kvs[j].v && kvs[i].k < kvs[j].k)
+	})
+}
+
+func getFirstUpTo(kvs []kv, toIdx int) []string {
+	n := toIdx
+	if n > len(kvs) {
+		n = len(kvs)
+	}
+
+	res := make([]string, 0, n)
+	for i := 0; i < n; i++ {
+		if kvs[i].v == 0 {
+			continue
+		}
+
+		res = append(res, kvs[i].k)
+	}
+
+	return res
 }
--- hw03_frequency_analysis/top_test.go
+++ hw03_frequency_analysis/top_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 // Change to true if needed.
-var taskWithAsteriskIsCompleted = false
+var taskWithAsteriskIsCompleted = true
 
 var text = `Как видите, он  спускается  по  лестнице  вслед  за  своим
 	другом   Кристофером   Робином,   головой   вниз,  пересчитывая
@@ -46,6 +46,19 @@ var text = `Как видите, он  спускается  по  лестни�
 func TestTop10(t *testing.T) {
 	t.Run("no words in empty string", func(t *testing.T) {
 		require.Len(t, Top10(""), 0)
+	})
+
+	t.Run("readme test", func(t *testing.T) {
+		expected := []string{
+			"and",     // 2
+			"one",     // 2
+			"cat",     // 1
+			"cats",    // 1
+			"dog,",    // 1
+			"dog,two", // 1
+			"man",     // 1
+		}
+		require.Equal(t, expected, Top10("cat and dog, one dog,two cats and one man"))
 	})
 
 	t.Run("positive test", func(t *testing.T) {

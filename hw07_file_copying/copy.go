902d0ced03c2b39c40946b194a02ae48f6dce8af
Ilya Sitnikov <sitnikovik@yandex.ru>
2024-07-21 18:05:44 +0300

    add copy implemenation

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"errors"
+	"io"
+	"os"
 )
 
 var (
@@ -10,6 +12,51 @@ var (
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	if offset < 0 {
+		return ErrOffsetExceedsFileSize
+	}
+
+	fromFile, err := os.Open(fromPath)
+	if err != nil {
+		return err
+	}
+	defer fromFile.Close()
+
+	toFile, err := os.Create(toPath)
+	if err != nil {
+		return err
+	}
+
+	stat, err := fromFile.Stat()
+	if err != nil {
+		return err
+	}
+	if stat.IsDir() {
+		return ErrUnsupportedFile
+	}
+
+	if offset > stat.Size() {
+		return ErrOffsetExceedsFileSize
+	}
+
+	if limit == 0 {
+		limit = stat.Size() - offset
+	}
+
+	if _, err := fromFile.Seek(offset, 0); err != nil {
+		return err
+	}
+
+	if _, err := io.CopyN(toFile, fromFile, limit); err != nil {
+		if err == io.EOF {
+			return nil
+		}
+		return err
+	}
+
+	if err := toFile.Close(); err != nil {
+		return err
+	}
+
 	return nil
 }

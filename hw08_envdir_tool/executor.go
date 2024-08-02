7e67ae147f745a428bd33f0ea32c328df8234ed5
Ilya Sitnikov <sitnikovik@yandex.ru>
2024-08-02 23:03:18 +0300

    executor refactored

--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -1,38 +1,83 @@
 package main
 
 import (
-	"fmt"
+	"errors"
 	"os"
 	"os/exec"
 )
 
+const (
+	// OkCode is a return code for success
+	OkCode = 0
+	// ErrCode is a return code for errors
+	ErrCode = 1
+)
+
+var (
+	// ErrNilEnvironment says that environment is nil
+	ErrNilEnvironment = errors.New("environment is nil")
+)
+
 // RunCmd runs a command + arguments (cmd) with environment variables from env.
 func RunCmd(cmd []string, env Environment) (returnCode int) {
-	returnCode = 0
+	var err error
+	returnCode = OkCode
 
+	defer func() {
+		if err != nil {
+			logError(err)
+		}
+	}()
+
+	if env == nil {
+		err = ErrNilEnvironment
+		returnCode = ErrCode
+		return
+	}
+	if err = setEnvironment(env); err != nil {
+		returnCode = ErrCode
+		return
+	}
 	if len(cmd) == 0 {
 		return
 	}
 
-	// Set environment variables
-	for k, v := range env {
-		if v.NeedRemove {
-			os.Unsetenv(k)
-		}
-		os.Setenv(k, v.Value)
-	}
-
-	// Run command
 	args := []string{}
 	if len(cmd) > 1 {
 		args = cmd[1:]
 	}
-	command := exec.Command(cmd[0], args...)
-	bb, err := command.Output()
-	if err != nil {
-		returnCode = 1
+	if err = run(cmd[0], args); err != nil {
+		returnCode = ErrCode
 	}
-	fmt.Print(string(bb))
 
 	return
+}
+
+func logError(err error) {
+	if err == nil {
+		return
+	}
+
+	os.Stderr.WriteString(err.Error())
+}
+
+func setEnvironment(env Environment) (err error) {
+	for k, v := range env {
+		if v.NeedRemove && os.Unsetenv(k) != nil {
+			return
+		}
+		if err = os.Setenv(k, v.Value); err != nil {
+			return err
+		}
+	}
+
+	return
+}
+
+func run(cmd string, args []string) error {
+	command := exec.Command(cmd, args...)
+	command.Stdout = os.Stdout
+	command.Stderr = os.Stderr
+
+	return command.Run()
 }

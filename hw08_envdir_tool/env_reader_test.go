5de27e4cc705f14b90220472294ab457dad89388
Ilya Sitnikov <sitnikovik@yandex.ru>
2024-08-02 10:04:32 +0300

    Add tests for ReadDir method

--- hw08_envdir_tool/env_reader_test.go
+++ hw08_envdir_tool/env_reader_test.go
@@ -1,7 +1,106 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestReadDir(t *testing.T) {
-	// Place your code here
+
+	type args struct {
+		path string
+	}
+	tests := []struct {
+		name string
+		args args
+
+		envPreset []string // Envnames to be set before targer func done to check NeedRemove flag for env vars parsed
+
+		want    Environment
+		wantErr bool
+	}{
+		{
+			name: "err on filepath specified",
+			args: args{
+				path: "testdata/env/BAR",
+			},
+			wantErr: true,
+		},
+		{
+			name: "err on unexisting dir specified",
+			args: args{
+				path: "testdata/envdir",
+			},
+			wantErr: true,
+		},
+		{
+			name: "ok on testdata envdir specified",
+			args: args{
+				path: "testdata/env",
+			},
+			want: Environment{
+				"BAR": EnvValue{
+					Value: "bar",
+				},
+				"EMPTY": EnvValue{
+					Value: "",
+				},
+				"FOO": EnvValue{
+					Value: "   foo\nwith new line",
+				},
+				"HELLO": EnvValue{
+					Value: "\"hello\"",
+				},
+				"UNSET": EnvValue{
+					Value: "",
+				},
+			},
+		},
+		{
+			name: "ok on testdata envdir specified with env vars preset before",
+			args: args{
+				path: "testdata/env",
+			},
+			envPreset: []string{"BAR", "UNSET"},
+			want: Environment{
+				"BAR": EnvValue{
+					Value:      "bar",
+					NeedRemove: true,
+				},
+				"EMPTY": EnvValue{
+					Value: "",
+				},
+				"FOO": EnvValue{
+					Value: "   foo\nwith new line",
+				},
+				"HELLO": EnvValue{
+					Value: "\"hello\"",
+				},
+				"UNSET": EnvValue{
+					Value:      "",
+					NeedRemove: true,
+				},
+			},
+		},
+	}
+
+	for i := 0; i < len(tests); i++ {
+		tt := tests[i]
+		t.Run(tt.name, func(t *testing.T) {
+			for _, v := range tt.envPreset {
+				os.Setenv(v, "")
+			}
+
+			got, err := ReadDir(tt.args.path)
+
+			if tt.wantErr {
+				require.Error(t, err)
+				require.Nil(t, got)
+				return
+			}
+			require.Equal(t, tt.want, got)
+		})
+	}
 }
--- hw08_envdir_tool/go.mod
+++ hw08_envdir_tool/go.mod
@@ -2,8 +2,10 @@ module github.com/sitnikovik/otus-golang-professional/hw08_envdir_tool
 
 go 1.22
 
+require github.com/stretchr/testify v1.9.0
+
 require (
-	github.com/inconshreveable/mousetrap v1.1.0 // indirect
-	github.com/spf13/cobra v1.8.1 // indirect
-	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )

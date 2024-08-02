d72757a46110f969b7f7b4a050e58c549aab2b51
Ilya Sitnikov <sitnikovik@yandex.ru>
2024-08-02 09:41:19 +0300

    Executor to set env vars implemented

--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -1,7 +1,38 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
+
 // RunCmd runs a command + arguments (cmd) with environment variables from env.
 func RunCmd(cmd []string, env Environment) (returnCode int) {
-	// Place your code here.
+	returnCode = 0
+
+	if len(cmd) == 0 {
+		return
+	}
+
+	// Set environment variables
+	for k, v := range env {
+		if v.NeedRemove {
+			os.Unsetenv(k)
+		}
+		os.Setenv(k, v.Value)
+	}
+
+	// Run command
+	args := []string{}
+	if len(cmd) > 1 {
+		args = cmd[1:]
+	}
+	command := exec.Command(cmd[0], args...)
+	bb, err := command.Output()
+	if err != nil {
+		returnCode = 1
+	}
+	fmt.Print(string(bb))
+
 	return
 }

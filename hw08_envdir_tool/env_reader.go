7e3ad856c2a043defae0f0fecfeeea24bfaaa9a4
Ilya Sitnikov <sitnikovik@yandex.ru>
2024-08-02 09:41:01 +0300

    Dir reader implemented

--- hw08_envdir_tool/env_reader.go
+++ hw08_envdir_tool/env_reader.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"bytes"
+	"fmt"
+	"os"
+	"strings"
+)
+
 type Environment map[string]EnvValue
 
 // EnvValue helps to distinguish between empty files and files with the first empty line.
@@ -11,6 +18,48 @@ type EnvValue struct {
 // ReadDir reads a specified directory and returns map of env variables.
 // Variables represented as files where filename is name of variable, file first line is a value.
 func ReadDir(dir string) (Environment, error) {
-	// Place your code here
-	return nil, nil
+	scandir, err := os.ReadDir(dir)
+	if err != nil {
+		return nil, err
+	}
+
+	env := make(Environment)
+	for _, file := range scandir {
+		if file.IsDir() {
+			continue
+		}
+
+		envName := file.Name()
+		if strings.Contains(envName, "=") {
+			return nil, fmt.Errorf("env name '%s' contains '='", envName)
+		}
+
+		bb, err := os.ReadFile(dir + "/" + envName)
+		if err != nil {
+			return nil, err
+		}
+
+		// терминальные нули (0x00) заменяются на перевод строки (\n)
+		isToShiftString := !bytes.Contains(bb, []byte{0x00})
+		bb = bytes.ReplaceAll(bb, []byte{0x00}, []byte{'\n'})
+
+		s := string(bb)
+		if isToShiftString {
+			for i := 0; i < len(s); i++ {
+				if s[i] == '\n' {
+					s = s[:i]
+					break
+				}
+			}
+		}
+		s = strings.Trim(strings.TrimRight(s, " "), "\t")
+
+		_, ok := os.LookupEnv(envName)
+		env[envName] = EnvValue{
+			Value:      s,
+			NeedRemove: ok,
+		}
+	}
+
+	return env, nil
 }

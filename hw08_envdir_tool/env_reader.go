ecc524c7d82494aac50c27587e6f19180fd29625
Ilya Sitnikov <31476786+sitnikovik@users.noreply.github.com>
2024-08-14 19:49:53 +0300

    Hw08 envdir tool (#8)

--- hw08_envdir_tool/env_reader.go
+++ hw08_envdir_tool/env_reader.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"bytes"
+	"errors"
+	"io/fs"
+	"os"
+	"strings"
+)
+
+var (
+	// ErrNilDirEntry says that dir entry is nil.
+	ErrNilDirEntry = errors.New("dirEntry is nil")
+	// ErrInvalidEnvName says that env name contains invalid characters.
+	ErrInvalidEnvName = errors.New("invalid env name")
+)
+
 type Environment map[string]EnvValue
 
 // EnvValue helps to distinguish between empty files and files with the first empty line.
@@ -8,9 +23,91 @@ type EnvValue struct {
 	NeedRemove bool
 }
 
+// Exists checks if the env variable already exists.
+func (e Environment) Exists(envName string) bool {
+	_, ok := e[envName]
+	return ok
+}
+
 // ReadDir reads a specified directory and returns map of env variables.
 // Variables represented as files where filename is name of variable, file first line is a value.
 func ReadDir(dir string) (Environment, error) {
-	// Place your code here
-	return nil, nil
+	dirEntries, err := os.ReadDir(dir)
+	if err != nil {
+		return nil, err
+	}
+
+	env := parseCurrentEnvironment()
+
+	for _, dirEntry := range dirEntries {
+		err := validateDirEntry(dirEntry)
+		if err != nil {
+			return nil, err
+		}
+
+		envName := dirEntry.Name()
+
+		bb, err := os.ReadFile(dir + "/" + envName)
+		if err != nil {
+			return nil, err
+		}
+
+		env[envName] = EnvValue{
+			Value:      prepareEnvValue(bb),
+			NeedRemove: env.Exists(envName),
+		}
+	}
+
+	return env, nil
+}
+
+func parseCurrentEnvironment() Environment {
+	env := make(Environment)
+	for _, envLine := range os.Environ() {
+		pair := strings.SplitN(envLine, "=", 2)
+		env[pair[0]] = EnvValue{
+			Value: pair[1],
+		}
+	}
+
+	return env
+}
+
+// validateDirEntry checks if dirEntry could be used as env variable.
+func validateDirEntry(dirEntry fs.DirEntry) error {
+	if dirEntry == nil {
+		return ErrNilDirEntry
+	}
+	if dirEntry.IsDir() {
+		return nil
+	}
+
+	envName := dirEntry.Name()
+	if strings.Contains(envName, "=") {
+		return ErrInvalidEnvName
+	}
+
+	return nil
+}
+
+// prepareEnvValue prepares env value from file content.
+func prepareEnvValue(bb []byte) string {
+	isToShiftString := !bytes.Contains(bb, []byte{0x00})
+	bb = bytes.ReplaceAll(bb, []byte{0x00}, []byte{'\n'})
+
+	s := string(bb)
+	if isToShiftString {
+		// shift string to the first '\n'
+		for i := 0; i < len(s); i++ {
+			if s[i] == '\n' {
+				// there is no need to shift with strings.Split()
+				s = s[:i]
+				break
+			}
+		}
+	}
+	s = strings.TrimRight(s, " ")
+	s = strings.Trim(s, "\t")
+
+	return s
 }
--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -1,7 +1,85 @@
 package main
 
+import (
+	"errors"
+	"os"
+	"os/exec"
+)
+
+const (
+	// OkCode is a return code for success.
+	OkCode = 0
+	// ErrCode is a return code for errors.
+	ErrCode = 1
+)
+
+// ErrNilEnvironment says that environment is nil.
+var ErrNilEnvironment = errors.New("environment is nil")
+
 // RunCmd runs a command + arguments (cmd) with environment variables from env.
 func RunCmd(cmd []string, env Environment) (returnCode int) {
-	// Place your code here.
+	var err error
+	returnCode = OkCode
+
+	defer func() {
+		if err != nil {
+			logError(err)
+		}
+	}()
+
+	if env == nil {
+		err = ErrNilEnvironment
+		returnCode = ErrCode
+		return
+	}
+	if err = setEnvironment(env); err != nil {
+		returnCode = ErrCode
+		return
+	}
+	if len(cmd) == 0 {
+		return
+	}
+
+	args := []string{}
+	if len(cmd) > 1 {
+		args = cmd[1:]
+	}
+	if err = run(cmd[0], args); err != nil {
+		returnCode = ErrCode
+	}
+
 	return
+}
+
+// logError writes error to stderr.
+func logError(err error) {
+	if err == nil {
+		return
+	}
+
+	os.Stderr.WriteString(err.Error())
+}
+
+// setEnvironment sets environment variables from env.
+func setEnvironment(env Environment) (err error) {
+	for k, v := range env {
+		if v.NeedRemove && os.Unsetenv(k) != nil {
+			return
+		}
+		if err = os.Setenv(k, v.Value); err != nil {
+			return err
+		}
+	}
+
+	return
+}
+
+// run runs a command with arguments.
+func run(cmd string, args []string) error {
+	command := exec.Command(cmd, args...)
+	command.Stdin = os.Stdin
+	command.Stdout = os.Stdout
+	command.Stderr = os.Stderr
+
+	return command.Run()
 }

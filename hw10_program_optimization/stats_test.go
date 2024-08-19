710ec48858923105ff9a24f3b5eedc17bbc1be60
Ilya Sitnikov <sitnikovik@yandex.ru>
2024-08-19 18:45:40 +0300

    add bench

--- hw10_program_optimization/stats_test.go
+++ hw10_program_optimization/stats_test.go
@@ -37,24 +37,30 @@ func TestGetDomainStat(t *testing.T) {
 		require.NoError(t, err)
 		require.Equal(t, DomainStat{}, result)
 	})
+}
 
-	t.Run("err on invalid json", func(t *testing.T) {
-		_, err := GetDomainStat(bytes.NewBufferString("some string"), "")
-		require.Error(t, err)
+func BenchmarkGetDomainStat(b *testing.B) {
+	data := `{"Id":1,"Name":"Howard Mendoza","Username":"0Oliver","Email":"aliquid_qui_ea@Browsedrive.gov","Phone":"6-866-899-36-79","Password":"InAQJvsq","Address":"Blackbird Place 25"}
+{"Id":2,"Name":"Jesse Vasquez","Username":"qRichardson","Email":"mLynch@broWsecat.com","Phone":"9-373-949-64-00","Password":"SiZLeNSGn","Address":"Fulton Hill 80"}
+{"Id":3,"Name":"Clarence Olson","Username":"RachelAdams","Email":"RoseSmith@Browsecat.com","Phone":"988-48-97","Password":"71kuz3gA5w","Address":"Monterey Park 39"}
+{"Id":4,"Name":"Gregory Reid","Username":"tButler","Email":"5Moore@Teklist.net","Phone":"520-04-16","Password":"r639qLNu","Address":"Sunfield Park 20"}
+{"Id":5,"Name":"Janice Rose","Username":"KeithHart","Email":"nulla@Linktype.com","Phone":"146-91-01","Password":"acSBF5","Address":"Russell Trail 61"}`
+
+	b.Run("find 'com'", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			_, _ = GetDomainStat(bytes.NewBufferString(data), "com")
+		}
 	})
 
-	t.Run("err on json items splitted with comma", func(t *testing.T) {
-		data := `{"Email":"aliquid_qui_ea@Browsedrive.gov"},
-{"Email":"mLynch@broWsecat.com"}`
-
-		_, err := GetDomainStat(bytes.NewBufferString(data), "")
-		require.Error(t, err)
+	b.Run("find 'gov'", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			_, _ = GetDomainStat(bytes.NewBufferString(data), "gov")
+		}
 	})
 
-	t.Run("no err on empty string", func(t *testing.T) {
-		got, err := GetDomainStat(bytes.NewBufferString(""), "")
-
-		require.NoError(t, err)
-		require.Equal(t, DomainStat{}, got)
+	b.Run("find 'unknown'", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			_, _ = GetDomainStat(bytes.NewBufferString(data), "unknown")
+		}
 	})
 }

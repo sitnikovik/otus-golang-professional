c11d70c29b7dde4a59d65adc8d690d6c8849904d
Ilya Sitnikov <sitnikovik@yandex.ru>
2024-09-09 09:33:27 +0300

    refactor input parsing

--- hw11_telnet_client/input.go
+++ hw11_telnet_client/input.go
@@ -23,37 +23,59 @@ func ParseInput(args []string) (*Args, *Flags, error) {
 
 	for i := 1; i < len(args); i++ {
 		arg := args[i]
-
-		// Parse arguments
-		if i < 3 && !strings.Contains(arg, "=") {
-			if i == 1 {
-				aa.Address = arg
-			}
-			if i == 2 {
-				port, err := strconv.Atoi(arg)
-				if err != nil {
-					return nil, nil, fmt.Errorf("port err: %w", err)
-				}
-				aa.Port = port
-			}
-
+		if arg == "" {
 			continue
 		}
 
 		// Parse flags
-		if strings.Contains(arg, "=") {
-			parts := strings.SplitN(arg, "=", 2)
-			key, value := parts[0], parts[1]
+		flagParsed, err := parseFlag(ff, arg)
+		if err != nil {
+			return nil, nil, err
+		}
+		if flagParsed {
+			continue
+		}
 
-			if key == "--timeout" {
-				ttl, err := strconv.Atoi(value)
-				if err != nil {
-					return nil, nil, fmt.Errorf("timeout err: %w", err)
-				}
-				ff.Timeout = ttl
-			}
+		// Parse arguments
+		err = parseArg(aa, arg)
+		if err != nil {
+			return nil, nil, err
 		}
 	}
 
 	return aa, ff, nil
+}
+
+func parseFlag(flags *Flags, in string) (bool, error) {
+	if !strings.Contains(in, "=") {
+		return false, nil
+	}
+
+	parts := strings.SplitN(in, "=", 2)
+	key, val := parts[0], parts[1]
+
+	if key == "--timeout" {
+		ttl, err := strconv.Atoi(val)
+		if err != nil {
+			return false, fmt.Errorf("timeout err: %w", err)
+		}
+		flags.Timeout = ttl
+		return true, nil
+	}
+
+	return false, nil
+}
+
+func parseArg(args *Args, in string) error {
+	if args.Address == "" {
+		args.Address = in
+		return nil
+	}
+
+	port, err := strconv.Atoi(in)
+	if err != nil {
+		return fmt.Errorf("port err: %w", err)
+	}
+	args.Port = port
+	return nil
 }

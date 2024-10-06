ed041936c531dc6c0bdb00d506499dabca81028f
Ilya Sitnikov <sitnikovik@yandex.ru>
2024-10-06 12:01:11 +0300

    added cache first impl

--- hw12_13_14_15_calendar/go.mod
+++ hw12_13_14_15_calendar/go.mod
@@ -2,4 +2,8 @@ module github.com/sitnikovik/otus-golang-professional/hw12_13_14_15_calendar
 
 go 1.22
 
-require gopkg.in/yaml.v3 v3.0.1 // indirect
+require (
+	github.com/go-redis/redis v6.15.9+incompatible // indirect
+	github.com/redis/go-redis v6.15.9+incompatible // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
--- hw12_13_14_15_calendar/internal/storage/memory/storage.go
+++ hw12_13_14_15_calendar/internal/storage/memory/storage.go
@@ -1,14 +1,46 @@
 package memorystorage
 
-import "sync"
+import (
+	"errors"
+	"fmt"
+	"sync"
 
-type Storage struct {
-	// TODO
+	"github.com/go-redis/redis"
+)
+
+// Storage describes
+type Storage interface {
+	// Get returns the value by key
+	Get(key string) (string, bool, error)
+}
+
+type storage struct {
 	mu sync.RWMutex //nolint:unused
+
+	redisClient *redis.Client
 }
 
-func New() *Storage {
-	return &Storage{}
+// New creates and returns the in-memory storage instance
+func New(redis *redis.Client) Storage {
+	return &storage{
+		redisClient: redis,
+	}
 }
 
-// TODO
+// Get returns the value by key
+func (s *storage) Get(key string) (string, bool, error) {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+
+	cmd := s.redisClient.Get(key)
+	if cmd == nil {
+		return "", false, fmt.Errorf("redis cmd is nil")
+	}
+
+	v, err := cmd.Result()
+	if errors.Is(err, redis.Nil) {
+		return "", false, nil
+	}
+
+	return v, true, nil
+}

4db4bc2676cfafac8871072e3543f16fb9118443
Ilya Sitnikov <sitnikovik@yandex.ru>
2024-10-12 09:22:28 +0300

    upd conf with redis and pg

--- hw12_13_14_15_calendar/internal/config/config.go
+++ hw12_13_14_15_calendar/internal/config/config.go
@@ -9,7 +9,38 @@ import (
 // Config describes the app configuration
 type Config struct {
 	// Logger describes the logging configuration
-	Logger LoggerConf
+	Logger LoggerConf `yaml:"logger"`
+	// Redis describes the redis configuration
+	Redis RedisConf `yaml:"redis"`
+	// PG describes the postgres configuration
+	PG PGConf `yaml:"pg"`
+}
+
+// LoggerConf describes the logging configuration
+type LoggerConf struct {
+	Level string `yaml:"level"`
+}
+
+// RedisConf describes the redis configuration
+type RedisConf struct {
+	// Network describes the network type
+	Network string `yaml:"network"`
+	// Addr describes the redis address
+	Addr string `yaml:"addr"`
+}
+
+// PGConf describes the postgres configuration
+type PGConf struct {
+	// User describes the postgres user
+	User string `yaml:"user"`
+	// Password describes the postgres password
+	Password string `yaml:"password"`
+	// Database describes the postgres database
+	Database string `yaml:"database"`
+	// Host describes the postgres host
+	Host string `yaml:"host"`
+	// Port describes the postgres port
+	Port string `yaml:"port"`
 }
 
 // Load parses the app configuration with provided path and returns it

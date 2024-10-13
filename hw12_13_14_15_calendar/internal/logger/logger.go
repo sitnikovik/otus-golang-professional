d37b58ea230e3b3d6cd28e4694170dd686c72eb5
Ilya Sitnikov <sitnikovik@yandex.ru>
2024-10-13 15:53:39 +0300

    upd logger

--- hw12_13_14_15_calendar/internal/logger/logger.go
+++ hw12_13_14_15_calendar/internal/logger/logger.go
@@ -1,6 +1,7 @@
 package logger
 
 import (
+	"fmt"
 	"log/slog"
 )
 
@@ -13,11 +14,21 @@ func Initialize(l Level) {
 	level = l
 }
 
+// Debugf logs the debug with formatted message
+func Debugf(format string, a ...interface{}) {
+	Debug(fmt.Sprintf(format, a...))
+}
+
 // Debug logs the debug message
 func Debug(msg string) {
 	if level >= DebugLevel {
 		slog.Debug(msg)
 	}
+}
+
+// Infof logs the info with formatted message
+func Infof(format string, a ...interface{}) {
+	Info(fmt.Sprintf(format, a...))
 }
 
 // Info logs the info message
@@ -27,11 +38,21 @@ func Info(msg string) {
 	}
 }
 
+// Warningf logs the warning with formatted message
+func Warningf(format string, a ...interface{}) {
+	Warning(fmt.Sprintf(format, a...))
+}
+
 // Warning logs the warning message
 func Warning(msg string) {
 	if level >= WarningLevel {
 		slog.Warn(msg)
 	}
+}
+
+// Errorf logs the error with formatted message
+func Errorf(format string, a ...interface{}) {
+	Warning(fmt.Sprintf(format, a...))
 }
 
 // Error logs the error message
@@ -41,11 +62,21 @@ func Error(msg string) {
 	}
 }
 
+// Alertf logs the alert with formatted message
+func Alertf(format string, a ...interface{}) {
+	Alert(fmt.Sprintf(format, a...))
+}
+
 // Alert logs the alert message
 func Alert(msg string) {
 	if level >= AlertLevel {
 		slog.Error(msg) // slog.Alert is not available
 	}
+}
+
+// Critical logs the critical with formatted message
+func Criticalf(format string, a ...interface{}) {
+	Critical(fmt.Sprintf(format, a...))
 }
 
 // Critical logs the critical message

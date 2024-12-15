335fdea57a13521904a2acadad9a953e1ec5aebd
Ilya Sitnikov <sitnikovik@yandex.ru>
2024-12-15 17:23:40 +0300

    Rename middleware.go to logging_middleware.go and update function name to LoggingMiddleware

--- hw12_13_14_15_calendar/internal/server/http/middleware/logging_middleware.go
+++ hw12_13_14_15_calendar/internal/server/http/middleware/logging_middleware.go
@@ -1,4 +1,4 @@
-package http
+package middleware
 
 import (
 	"net/http"
@@ -6,8 +6,8 @@ import (
 	"github.com/sitnikovik/otus-golang-professional/hw12_13_14_15_calendar/internal/logger"
 )
 
-// loggingMiddleware is the middleware to log the request
-func loggingMiddleware(next http.HandlerFunc) http.HandlerFunc {
+// LoggingMiddleware is the middleware to log the request
+func LoggingMiddleware(next http.HandlerFunc) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		logger.Debugf("Request: %s %s", r.Method, r.URL.Path)
 		next(w, r)

176adb11f747ca51b550ca1b568dbd12ca19b630
Ilya Sitnikov <sitnikovik@yandex.ru>
2024-12-15 17:23:07 +0300

    Refactor App structure to use pointer for DIContainer and simplify DI access method

--- hw12_13_14_15_calendar/internal/app/app.go
+++ hw12_13_14_15_calendar/internal/app/app.go
@@ -1,21 +1,16 @@
 package app
 
-import (
-	"github.com/sitnikovik/otus-golang-professional/hw12_13_14_15_calendar/internal/app/depinjection"
-)
-
-type App struct { // TODO
-	di depinjection.DIContainer
-}
-
-// DIContainer returns the DI container instance
-func (a *App) DIContainer() depinjection.DIContainer {
-	return nil
+type App struct {
+	di *DIContainer
 }
 
 // New creates and returns the app instance
-func New(di depinjection.DIContainer) *App {
+func New(di *DIContainer) *App {
 	return &App{
 		di: di,
 	}
+}
+
+func (a *App) DI() *DIContainer {
+	return a.di
 }

8141c84d70351e6271385ade8c1da7d0c365ac26
Ilya Sitnikov <sitnikovik@yandex.ru>
2024-12-19 09:47:47 +0300

    Add migration script to create events table with necessary fields

---
+++ hw12_13_14_15_calendar/migrations/20241219060422_migrations_create_events_table.sql
@@ -0,0 +1,18 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE events
+(
+    id BIGSERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP NOT NULL default current_timestamp,
+    finished_at TIMESTAMP NOT NULL default current_timestamp,
+    description TEXT NOT NULL,
+    owner_id BIGINT NOT NULL,
+    notify_before INT NOT NULL
+)
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE events
+-- +goose StatementEnd

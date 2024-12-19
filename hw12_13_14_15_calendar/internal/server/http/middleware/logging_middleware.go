b1ab56e12a0a0d7016a6b1c92f3bcee97766f5fe
Ilya Sitnikov <sitnikovik@yandex.ru>
2024-12-19 22:37:07 +0300

    Enhance logging middleware to capture request details and response status

--- hw12_13_14_15_calendar/internal/server/http/middleware/logging_middleware.go
+++ hw12_13_14_15_calendar/internal/server/http/middleware/logging_middleware.go
@@ -2,6 +2,7 @@ package middleware
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/sitnikovik/otus-golang-professional/hw12_13_14_15_calendar/internal/logger"
 )
@@ -9,7 +10,43 @@ import (
 // LoggingMiddleware is the middleware to log the request.
 func LoggingMiddleware(next http.HandlerFunc) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		logger.Debugf("Request: %s %s", r.Method, r.URL.Path)
-		next(w, r)
+		start := time.Now()
+		// Create a response writer wrapper to capture the status code
+		wwrap := &responseWriter{w, http.StatusOK}
+
+		// Call the next handler
+		next(wwrap, r)
+
+		// Log the request details
+		clientIP := r.RemoteAddr
+		if ip := r.Header.Get("X-Real-IP"); ip != "" {
+			clientIP = ip
+		} else if ip = r.Header.Get("X-Forwarded-For"); ip != "" {
+			clientIP = ip
+		}
+
+		logger.Debugf(
+			"%s [%s] %s %s %s %d %s \"%s\"",
+			clientIP,
+			start.Format("02/Jan/2006:15:04:05 -0700"),
+			r.Method,
+			r.URL.Path,
+			r.Proto,
+			wwrap.statusCode,
+			time.Since(start),
+			r.UserAgent(),
+		)
 	})
+}
+
+// responseWriter is a wrapper around http.ResponseWriter that captures the status code.
+type responseWriter struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+// WriteHeader captures the status code.
+func (rw *responseWriter) WriteHeader(code int) {
+	rw.statusCode = code
+	rw.ResponseWriter.WriteHeader(code)
 }

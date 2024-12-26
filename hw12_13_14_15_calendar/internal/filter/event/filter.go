a24a0f3759a7646798921aafa12f96c21a6ab17c
Ilya Sitnikov <sitnikovik@yandex.ru>
2024-12-26 22:35:13 +0300

    Enhance event filter structure with date range and pagination support

--- hw12_13_14_15_calendar/internal/filter/event/filter.go
+++ hw12_13_14_15_calendar/internal/filter/event/filter.go
@@ -1,11 +1,30 @@
 package event
 
-import "reflect"
+import (
+	"reflect"
+	"time"
+)
 
 // Filter describes the filter for the list of events.
 type Filter struct {
-	IDs   []uint64
+	// CreatedFrom - the start date of the event creation.
+	CreatedFrom *time.Time
+	// CreatedTo - the end date of the event creation.
+	CreatedTo *time.Time
+	// FinishedFrom - the start date of the event finish.
+	FinishedFrom *time.Time
+	// FinishedTo - the end date of the event finish.
+	FinishedTo *time.Time
+
+	// IDs - the list of event IDs.
+	IDs []uint64
+	// OwnerIDs - the list of event owner IDs.
+	OwnerIDs []uint64
+
+	// Limit - the limit of the events.
 	Limit uint64
+	// Offset - the offset of the events for pagination.
+	Offset uint64
 }
 
 // Empty checks if the filter is full empty.

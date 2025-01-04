94889ebdf7be60678262c4a4be046c307223323d
Ilya Sitnikov <sitnikovik@yandex.ru>
2025-01-04 12:49:41 +0300

    Implement GetEventsForWeek gRPC method and add corresponding unit tests

---
+++ hw12_13_14_15_calendar/internal/api/calendar/v1/grpc/get_events_for_week.go
@@ -0,0 +1,26 @@
+package grpc
+
+import (
+	"context"
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
+	pkg "github.com/sitnikovik/otus-golang-professional/hw12_13_14_15_calendar/pkg/calendar/v1"
+)
+
+// GetEventsForToday returns a list of events that will occur today.
+func (i *Implementation) GetEventsForWeek(
+	ctx context.Context,
+	req *pkg.GetEventsForWeekRequest,
+) (*pkg.GetEventsResponse, error) {
+	events, err := i.eventService.GetEventsForWeek(ctx)
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+
+	return &pkg.GetEventsResponse{
+		Events: eventsToResponse(events),
+		Total:  uint64(len(events)),
+	}, nil
+}

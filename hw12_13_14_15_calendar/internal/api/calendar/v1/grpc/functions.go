a4e4817d851db41cd5eab1438f70b89a8f1ad184
Ilya Sitnikov <sitnikovik@yandex.ru>
2025-01-04 10:57:57 +0300

    Improve function comments for time conversion methods in gRPC

--- hw12_13_14_15_calendar/internal/api/calendar/v1/grpc/functions.go
+++ hw12_13_14_15_calendar/internal/api/calendar/v1/grpc/functions.go
@@ -6,12 +6,12 @@ import (
 	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
-// ToGRPCTime конвертирвет
+// ToGRPCTime converts time.Time to protobuf time
 func ToGRPCTime(t time.Time) *timestamppb.Timestamp {
 	return &timestamppb.Timestamp{Nanos: int32(t.Nanosecond()), Seconds: t.Unix()}
 }
 
-// FromGRPCTime конвертирует
+// FromGRPCTime converts protobuf time to time.Time
 func FromGRPCTime(t *timestamppb.Timestamp) time.Time {
 	return time.Unix(t.GetSeconds(), int64(t.GetNanos()))
 }

a944e9ad873201d8d8a5f3bc940d9a2a13ce66d6
Ilya Sitnikov <sitnikovik@yandex.ru>
2025-01-04 09:09:14 +0300

    Add integration test setup for PgStorage in storage_test.go

---
+++ hw12_13_14_15_calendar/internal/storage/event/pgsql/storage_test.go
@@ -0,0 +1,25 @@
+package pgsql
+
+import (
+	"strconv"
+	"testing"
+
+	"github.com/sitnikovik/otus-golang-professional/hw12_13_14_15_calendar/internal/config"
+	"github.com/sitnikovik/otus-golang-professional/hw12_13_14_15_calendar/internal/connections/pg"
+)
+
+// newTestPgStorage creates new PgStorage for integration testing
+func newTestPgStorage(t *testing.T) *PgStorage {
+	cfg, err := config.NewTestConfig()
+	if err != nil {
+		t.Fatalf("failed to create test config: %v", err)
+	}
+
+	pgPort, _ := strconv.Atoi(cfg.PG.Port)
+	conn, err := pg.NewConnPool(cfg.PG.Database, cfg.PG.User, cfg.PG.Password, cfg.PG.Host, pgPort)
+	if err != nil {
+		t.Fatalf("failed to create pg conn pool: %v", err)
+	}
+
+	return New(conn)
+}
